2fe40e9919e9e42ef882a77c0fc3751a0c866557
Waoooooooo <1185202122@qq.com>
2023-06-02 17:42:43 +0800

    213

---
+++ miao/lodash/waoooooooo-lodash.js
@@ -0,0 +1,37 @@
+
+var waoooooooo = {
+
+  /**
+   * 01 chunk
+   * 将数组（array）拆分成多个 size 长度的区块，并将这些区块组成一个新数组。
+   * 如果array 无法被分割成全部等长的区块，那么最后剩余的元素将组成一个区块。
+   * @param {*} ary array (Array): 需要处理的数组
+   * @param {*} size  [size=1] (number): 每个数组区块的长度
+   * @return  (Array): 返回一个包含拆分区块的新数组（注：相当于一个二维数组）。
+   */
+  chunk: function (ary, size) {
+
+    var result = []
+
+    for (let index = 0; index < ary.length; index++) {
+      var count = 0
+      var arr = []
+      for (let j = index; j < ary.length; j++) {
+        count++
+        arr.push(ary[j])
+        if (count == size) {
+          result.push(arr)
+          count = 0
+          index = j
+          break
+        }
+      }
+      if (count > 0) {
+        //數組遍歷完有剩余
+        result.push(arr)
+        break
+      }
+    }
+    return result
+  }
+}
