741349f14a22d931c58d61fb67c04d198dc6adf0
Waoooooooo <1185202122@qq.com>
2023-06-03 09:53:33 +0800

    123

--- miao/lodash/waoooooooo-lodash.js
+++ miao/lodash/waoooooooo-lodash.js
@@ -29,11 +29,17 @@ var waoooooooo = {
     * 02 compact
     *    创建一个新数组，包含原数组中所有的非假值元素。例如false, null,0, "",
     *    undefined, 和 NaN 都是被认为是“假值”。
-    * @param {*} array array (Array): 待处理的数组
-    * @return  (Array): 返回过滤掉假值的新数组。
+    * @param {Array} array : 待处理的数组
+    * @return{Array}: 返回过滤掉假值的新数组。
     */
-  compact: array => array.filter(i => i)
+  compact: array => array.filter(i => i),
+  /**
+   * 03
+   * @param {Array} array  被连接的数组。
+   * @param  {...any} args 连接的值
+   */
+  concat: function name(array,...args) {
 
-
+  }
 
 }
