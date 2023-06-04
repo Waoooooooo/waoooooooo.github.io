9493eef4d24c96de05d8989d5947287e8563b144
Waoooooooo <1185202122@qq.com>
2023-06-04 11:46:19 +0800

    1

--- miao/lodash/waoooooooo-lodash.js
+++ miao/lodash/waoooooooo-lodash.js
@@ -512,7 +512,7 @@ var waoooooooo = {
   nth: (array, n = 0) => n >= 0 ? array[n] : array[array.length + n],
 
   //移除数组array中所有和给定值相等的元素，使用SameValueZero 进行全等比较。
-  //some 或
+  //some 或 ||
   pull: (array, values) => array.filter(e => !values.some(val => waoooooooo.isEqual(e, val))),
 
   //深度全等方法(数组 对象 的值全等)
@@ -542,5 +542,8 @@ var waoooooooo = {
       return false
     }
   }
+
+
+
 }
 
